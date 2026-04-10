44fe9a2687c39d69005cd782bb8a975b0a3fd6d3
AR-JUN-G <arjun07091102@gmail.com>
2026-04-10 23:04:15 +0530

    process fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,9 @@
-declare var process: {
-  env: {
-    BASE_URL?: string;
-    SOCKET_URL?: string;
-    [key: string]: string | undefined;
-  };
-};
+declare var process: any;
 
-export const BASE_URL = process.env.BASE_URL || "http://localhost:7777/api";
-export const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:7777";
+export const BASE_URL = (typeof process !== "undefined" && process?.env?.BASE_URL) 
+    ? process.env.BASE_URL 
+    : "http://localhost:7777/api";
+
+export const SOCKET_URL = (typeof process !== "undefined" && process?.env?.SOCKET_URL) 
+    ? process.env.SOCKET_URL 
+    : "http://localhost:7777";
